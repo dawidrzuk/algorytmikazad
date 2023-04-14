d2b2bf8f8f9255d689292ef4554e6eb3c4d09316
OGMaksiu <mateckimaksymilian@gmail.com>
2023-04-14 14:45:11 +0200

    lps

--- app/src/main/java/com/example/algorytmikazad/MainActivity.kt
+++ app/src/main/java/com/example/algorytmikazad/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,19 +17,45 @@ class MainActivity : AppCompatActivity() {
         val bf = findViewById<TextView>(R.id.textView2)
         val br = findViewById<TextView>(R.id.textView3)
         val rk = findViewById<TextView>(R.id.textView4)
-        val wzorzec = findViewById<EditText>(R.id.editTextTextPersonName)
-        val dlugosc = findViewById<EditText>(R.id.editTextTextPersonName2)
+        val wzorzec = findViewById<EditText>(R.id.editTextTextPersonName4)
+        val dlugosc = findViewById<EditText>(R.id.editTextNumber2)
 
-        fun losowanietekstu(): String {
-            val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-            val outputStrLength = (1..dlugosc).shuffled().first()
-
-            return (1..dlugosc)
-                .map{ kotlin.random.Random.nextInt(0, charPool.size) }
+        fun losowanietekstu(ilosc: Int): String {
+            val charPool: List<Char> =
+                ('a'..'z') + ('A'..'Z') + ('0'..'9')
+            return (1..ilosc)
+                .map {
+                    Random.nextInt(
+                        0,
+                        charPool.size
+                    )
+                }
                 .map(charPool::get)
                 .joinToString("")
         }
-
+        val tekst = losowanietekstu(dlugosc.text.toString().toInt())
+        fun prefixsufix(wzorzec: String): IntArray {
+            val m = wzorzec.length
+            val lps = IntArray(m)
+            var len = 0
+            var i = 1
+            while (i < m) {
+                if (wzorzec[i] == wzorzec[len]) {
+                    len++
+                    lps[i] = len
+                    i++
+                } else {
+                    if (len != 0) {
+                        len = lps[len - 1]
+                    } else {
+                        lps[i] = 0
+                        i++
+                    }
+                }
+            }
+            return lps
+        }
 
     }
+
 }
